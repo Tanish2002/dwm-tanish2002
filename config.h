c6a78a8942f0a04b6a1d8c3cd61bdb9c12fd7259
Tanish Khare <tanishkhare@gmail.com>
2020-03-25 11:01:23 +0530

    Remade the build

--- config.h
+++ config.h
@@ -1,16 +1,15 @@
 /* -*--*-*-*-*-*-*-*-*- GAPS -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
-#define GAPS_START 30
-#define BORDERPX_START 0
+#define GAPS_START 22
+#define BORDERPX_START 3
+/* -*-*-*-*-*-*-*-*-*- SMALL GAPS *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- */
+/* #define GAPS_START 8 */
+/* #define BORDERPX_START 1 */
 /* -*-*-*-*-*-*-*-*-*- NO GAPS *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- */
 /* #define GAPS_START 0 */
 /* #define BORDERPX_START 1 */
 /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
 
 /* -*-*-*-*-*-*-*-*- FLOATING BAR -*-*-*-*-*-*-*-*-*-*-*-*-*-* */
-/*static const unsigned int gappih    = 10;     */  /* horiz inner gap between windows */
-/*static const unsigned int gappiv    = 10;     */  /* vert inner gap between windows */
-/*static const unsigned int gappoh    = 10;     */  /* horiz outer gap between windows and screen edge */
-/*static const unsigned int gappov    = 10;     */  /* vert outer gap between windows and screen edge */
 static const int vertpad     = GAPS_START - GAPS_START / 3; // vertical padding of bar
 static const int sidepad     = GAPS_START - GAPS_START / 3; // horizontal padding of bar
 static const int horizpadbar = 6;          // horizontal padding for statusbar
@@ -35,12 +34,32 @@ static const char*fonts[] = {
 static const Rule rules[] = {
     /* class          instance    title    tags mask  iscentered   isfloating   monitor */
     { "st-256color",  NULL,      0,             0,         1,           0,           -1 },
+<<<<<<< HEAD
+    { "tabbed",       NULL,      0,             0,         1,           0,           -1 },
+    { 0,              NULL,      "x9term",      0,         0,           1,           -1 },
+=======
     { 0,              NULL,      "draw",        0,         0,           1,           -1 },
+>>>>>>> 9bd5a0f08acf2389b6e14ba567518423edb6b960
     { 0,              NULL,      "floating-st", 0,         1,           1,           -1 },
     { "feh",          NULL,      0,             0,         1,           1,           -1 },
     { "mpv",          NULL,      0,             0,         1,           1,           -1 },
     { "mupdf",        NULL,      0,             0,         1,           0,           -1 },
     { "MuPDF",        NULL,      0,             0,         1,           0,           -1 },
+<<<<<<< HEAD
+    { "discord",      NULL,      0,        1 << 5,         1,           0,           -1 },
+};
+
+
+static const char norm_fg[] = "#d5d0c2"; 
+static const char norm_bg[] = "#3a3a3a"; 
+static const char norm_border[] = "#575757";
+static const char sel_fg[] = "#ffffff"; 
+static const char sel_bg[] = "#747474"; 
+static const char sel_border[] = "#3a3a3a"; 
+static const char urg_fg[] = "#d5d0c2"; 
+static const char urg_bg[] = "#3a3a3a"; 
+static const char urg_border[] = "#3a3a3a"; 
+=======
     { "discord",      NULL,      0,        1 << 5,         0,           1,           -1 },
 
 
@@ -57,12 +76,13 @@ static const char sel_border[] = "#d4ddda";
 static const char urg_fg[] = "#D5d0c2"; 
 static const char urg_bg[] = "#3a3a3a"; 
 static const char urg_border[] = "#45405F"; 
+>>>>>>> 9bd5a0f08acf2389b6e14ba567518423edb6b960
 static const char *colors[][3]      = { 
 /*               fg           bg         border                         */ 
-[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins 
-[SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border }, 
-}; 
+[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins */
+[SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win */
+[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+};
 
 /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
 /*
@@ -82,57 +102,59 @@ static const float mfact = 0.5;
 static const Layout layouts[] = {
     { "|  ", tile },
     { "| 缾 ", NULL }, // floating
-    { "|  ", monocle },
+    /* { "|  ", monocle }, */
 };
-#define TAGKEYS(KEY,TAG) { Mod4Mask, KEY, view, {.ui = 1 << TAG} }, \
-                         { Mod4Mask|ShiftMask, KEY, tag, {.ui = 1 << TAG} },
-#define SH(cmd) { .v = (const char*[]) { "/bin/zsh", "-c", cmd, NULL } }
+
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) { MODKEY, KEY, view, {.ui = 1 << TAG} }, \
+                         { MODKEY|ShiftMask, KEY, tag, {.ui = 1 << TAG} },
+#define SH(cmd) { .v = (const char*[]) { "/bin/sh", "-c", cmd, NULL } }
 static const char *term[] = { "st", NULL };
 #include <X11/XF86keysym.h>
 static Key keys[] = {
-    /* modifier            key        function       argument */
+    /* modifier            key                       function       argument */
     /* -*-*-*-*-*-*-*- programs -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
-    { Mod4Mask,             XK_Return, spawn,        SH("tabbed -d -c -r 2 st -w ''") },
-    { Mod4Mask|ShiftMask,   XK_Return, spawn,        { .v = term, }         },
-    { Mod4Mask,             XK_grave,  spawn,        SH("$HOME/bin/term_draw")         },
-    { Mod4Mask,             XK_d,      spawn,        SH("rofi -show drun") },
-    { Mod4Mask,             XK_n,      spawn,        SH("$HOME/bin/Theme-change") },
-    { Mod4Mask,             XK_c,      spawn,        SH("$HOME/bin/rofi-clip")             },
-    { 0,                    XK_Print,  spawn,        SH("$HOME/bin/screenshot-menu.sh")            },
-    /* { Mod1Mask|ShiftMask,   XK_w,      spawn,        SH("tabbed -d -c surf -e") }, */
-    { Mod4Mask,             XK_Delete, spawn,        SH("$HOME/bin/rofi-emoji")              },
-    { Mod1Mask|ControlMask, XK_k,      spawn,        SH("keylayout")        },
+    { MODKEY,             XK_Return,                 spawn,         SH("tabbed -d -c -r 2 st -w ''") },
+    { MODKEY|ShiftMask,   XK_Return,                 spawn,         { .v = term, }         },
+    { MODKEY,             XK_grave,                  spawn,         SH("$HOME/bin/term-draw")         },
+    { MODKEY,             XK_d,                      spawn,         SH("rofi -show drun") },
+    { MODKEY,             XK_n,                      spawn,         SH("$HOME/bin/Theme-change") },
+    { MODKEY,             XK_c,                      spawn,         SH("$HOME/bin/rofi-clip") },
+    { MODKEY,             XK_m,                      spawn,         SH("$HOME/bin/rofi_mpvtube") },
+    { 0,                  XK_Print,                  spawn,         SH("$HOME/bin/screenshot-menu.sh")             },
     /* -*-*-*-*-*-*-*- media control -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- */
-    { Mod4Mask,      XK_m,                        spawn,  SH("$HOME/bin/rofi_mpvtube") },  
-    { 0,             XF86XK_AudioRaiseVolume,     spawn,  SH("amixer sset Master 5%+") },
-    { 0,             XF86XK_AudioLowerVolume,     spawn,  SH("amixer sset Master 5%-") },
-    { 0,             XF86XK_AudioMute,            spawn,  SH("amixer sset Master toggle") },
-    { 0,             XF86XK_AudioPlay,            spawn,  SH("$HOME/bin/mpvctl -t")   },
-    { 0,             XF86XK_AudioNext,            spawn,  SH("$HOME/bin/mpvctl -n")   },
-    { 0,             XF86XK_AudioPrev,            spawn,  SH("$HOME/bin/mpvctl -p")   },
+    { 0,                  XF86XK_AudioRaiseVolume,   spawn,         SH("amixer sset Master 5%+") },
+    { 0,                  XF86XK_AudioLowerVolume,   spawn,         SH("amixer sset Master 5%-") },
+    { 0,                  XF86XK_AudioMute,          spawn,         SH("amixer sset Master toggle")   },
+    { 0,                  XF86XK_AudioPlay,          spawn,         SH("$HOME/bin/mpvctl -t")   },
+    { 0,                  XF86XK_AudioNext,          spawn,         SH("pkill skroll ; $HOME/bin/mpvctl -n")   },
+    { 0,                  XF86XK_AudioPrev,          spawn,         SH("pkill skroll ; $HOME/bin/mpvctl -p")   },
+    { 0,                  XF86XK_MonBrightnessUp,    spawn,         SH("xbacklight -inc 10")   },
+    { 0,                  XF86XK_MonBrightnessDown,  spawn,         SH("xbacklight -dec 10")   },
     /* -*-*-*-*-*-*-*- dwm commands -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
-    { Mod1Mask,            XK_space,  togglescratch, SH("st -t scratchpad -g 68x18") },
-    { Mod4Mask|ShiftMask,  XK_q,      killclient,    {0} },
-    { Mod4Mask,            XK_Tab,    focusstack,    {.i = +1 } },
-    { Mod4Mask|ShiftMask,  XK_Left,   setmfact,      {.f = -0.05} },
-    { Mod4Mask|ShiftMask,  XK_Right,  setmfact,      {.f = +0.05} },
-    { Mod4Mask,            XK_t,      setlayout,     {.v = &layouts[0]} },
-    { Mod4Mask,            XK_s,      setlayout,     {.v = &layouts[1]} },
-    { Mod4Mask|ShiftMask,  XK_m,      setlayout,     {.v = &layouts[2]} },
-    { Mod4Mask,            XK_b,      togglebar,     {0} },
-    { Mod4Mask|ShiftMask,  XK_s,      togglesticky,  {0} },
-    { Mod4Mask|ShiftMask,  XK_space,  togglefloating,{0} },
-    { Mod4Mask,            XK_h,      rotatestack,   {.i = -1 } },
-    { Mod4Mask,            XK_l,      rotatestack,   {.i = +1 } },
-    { Mod4Mask,            XK_Tab,    view,          {0} },
-    { Mod4Mask|ShiftMask,  XK_Escape, quit,          {0} },
-    { Mod4Mask|ShiftMask,  XK_r,      quit,          {1} },
+    { MODKEY,            XK_space,                   togglescratch, SH("st -t scratchpad -g 68x18") },
+    { MODKEY|ShiftMask,  XK_q,                       killclient,    {0} },
+    { MODKEY,            XK_Tab,                       focusstack,    {.i = +1 } },
+/*    { MODKEY,            XK_k,                       focusstack,    {.i = -1 } }, */
+    { MODKEY|ShiftMask,  XK_h,                       setmfact,      {.f = -0.05} },
+    { MODKEY|ShiftMask,  XK_l,                       setmfact,      {.f = +0.05} },
+    { MODKEY,            XK_t,                       setlayout,     {.v = &layouts[0]} },
+    { MODKEY,            XK_s,                       setlayout,     {.v = &layouts[1]} },
+    /* { MODKEY,            XK_m,                       setlayout,     {.v = &layouts[2]} }, */
+    { MODKEY,            XK_b,                       togglebar,     {0} },
+    { MODKEY|ShiftMask,  XK_s,                       togglesticky,  {0} },
+    { MODKEY|ShiftMask,  XK_space,                   togglefloating,{0} },
+    { MODKEY,            XK_h,                       rotatestack,   {.i = -1 } },
+    { MODKEY,            XK_l,                       rotatestack,   {.i = +1 } },
+/*    { MODKEY,            XK_Tab,                     view,          {0} }, */
+    { MODKEY|ShiftMask,  XK_Escape,                  quit,           {0} },
+    { MODKEY|ShiftMask,  XK_r,                       quit,           {1} },
     /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
-    { Mod4Mask|ShiftMask,  XK_Up,     setsmfact,      {.f = +0.05} },
-    { Mod4Mask|ShiftMask,  XK_Down,   setsmfact,      {.f = -0.05} },
-    { Mod4Mask,            XK_g,      setgaps,        {.i = +4}    },
-    { Mod4Mask|ShiftMask,  XK_g,      setgaps,        {.i = -4}    },
-    { Mod4Mask,            XK_f,      togglefullscr,  {0} },
+    { MODKEY|ShiftMask,  XK_k,      setsmfact,      {.f = +0.05} },
+    { MODKEY|ShiftMask,  XK_j,      setsmfact,      {.f = -0.05} },
+    { MODKEY,            XK_g,      setgaps,        {.i = +4}    },
+    { MODKEY|ShiftMask,  XK_g,      setgaps,        {.i = -4}    },
+    { MODKEY,            XK_f,      togglefullscr,  {0} },
     /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
     TAGKEYS(XK_1,0) TAGKEYS(XK_2,1) TAGKEYS(XK_3,2) TAGKEYS(XK_4,3) 
     TAGKEYS(XK_5,4) TAGKEYS(XK_6,5) 
@@ -143,9 +165,16 @@ static Key keys[] = {
 };
 
 static Button buttons[] = {
+<<<<<<< HEAD
+    { ClkClientWin,  MODKEY,  Button1, movemouse,   {0} },
+    { ClkClientWin,  MODKEY,  Button3, resizemouse, {0} },
+    { ClkRootWin,    0,       Button3, spawn,       SH("x9term") }, // p9 rio style terminal drawing
+	{ ClkTagBar,     0,       Button1,        view,           {0} },
+=======
     { ClkClientWin,         Mod1Mask,       Button1,        movemouse,      {0} },
     { ClkClientWin,         Mod1Mask,       Button3,        resizemouse,    {0} },
     { ClkRootWin,           0,              Button3,        spawn,          SH("x9term") },
+>>>>>>> 9bd5a0f08acf2389b6e14ba567518423edb6b960
 };
 
 static unsigned int gappx = GAPS_START;
@@ -156,6 +185,6 @@ static const unsigned int minwsz = 20; /* min height of a client for smfact */
 /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
 static const float smfact     = 0.00; /* factor of tiled clients [0.00..0.95] */
 static const int resizehints  = 1;
-static const int focusonwheel = 0;
+static const int focusonwheel = 1;
 static const char scratchpadname[] = "scratchpad";
 static const int nmaster      = 1;
